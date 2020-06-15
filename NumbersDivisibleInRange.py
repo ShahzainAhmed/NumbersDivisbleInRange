6336a7f6c7cee059eaae9725c887ffed65b1042a
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2020-06-15 09:16:22 +0500

    s

--- NumbersDivisibleInRange.py
+++ NumbersDivisibleInRange.py
@@ -1,5 +1,7 @@
 # Taking input of lower range from the user.
 lower=int(input("Enter lower range limit: "))
+
+# Taking input of upper range from the user.
 upper=int(input("Enter upper range limit: "))
 n=int(input("Enter the number to be divided by: "))
 for i in range(lower,upper+1):
